f3b2c7cd52584384fcb86a9f9c5a473b3194349f
root <root@controlla.(none)>
2013-10-31 02:29:53 +0000

    piles of trash

--- cayenne/app/app.rb
+++ cayenne/app/app.rb
@@ -74,7 +74,7 @@ post "/workflows" do
   workflow_request = JSON.parse(request.body.read)
   engine = Cayenne::Engine::WorkFlow.new
   workflow_request = engine.validate_request(workflow_request)
-  error_json = {"error" => nil, "code" => workflow_request[0]}
+  response_json = {"response" => nil, "code" => workflow_request[0]}
   status workflow_request[0]
   
 =begin
@@ -84,15 +84,59 @@ validate method returns 2 element array, 0th element will be return code
 
   if workflow_request[0] == 202
     workflow_request = workflow_request[1]['workflow']
-    workflow = WorkFlow.create(
+    @workflow = WorkFlow.create(
       :workflow_props => workflow_request['workflow_props'],
       :name => workflow_request['name'],
       :job_ids => workflow_request['job_ids']
     )
   else
-    error_json['error'] = workflow_request[1]
-    body error_json.to_json
+    response_json['response'] = workflow_request[1]
+    body response_json.to_json
   end
 
+end
+
+=begin
+Delete a workflow
+=end
+delete "/workflows/:id" do
+  response_json = {'response' => nil, 'code' => nil}
+  begin
+    @workflow = WorkFlow.get(params[:id])
+    @workflow.destroy
+    status 204
+    response_json['response'] = "Workflow deleted"
+    response_json['code'] = 204
+    body response_json.to_json
+  rescue
+    status 404
+    response_json['response'] = "workflow does not exist"
+    response_json['code'] = 404
+    body response_json.to_json
+  end
+end
+
+=begin
+ Update a workflow
+=end
+
+put "/workflows/:id" do
+  response_json = {}
+  engine = Cayenne::Engine::WorkFlow.new
+  workflow_request = JSON.parse(request.body.read)
+  workflow_request = engine.validate_request(workflow_request)
+  response_json['code'] = workflow_request[0]
+  puts workflow_request[1]
+  if workflow_request[0] == 202
+    begin
+      @workflow = WorkFlow.get(params[:id])
+      @workflow.update(workflow_request[1]['workflow'])
+    rescue
+      status 404
+      response_json['response'] = workflow_request[1]
+      response_json['code'] = 404
+      body response_json.to_json
+    end
+  end
 end
   
--- cayenne/engine/engine.rb
+++ cayenne/engine/engine.rb
@@ -10,31 +10,36 @@ class Cayenne
       attr_accessor :workflow_props, :workflow_id, :update_at, :created_at, :job_ids, :name
      
       def validate_request(workflow_request)
-        if  workflow_request.has_key?('workflow')
-          #if no name attr specfied 
-          if ! workflow_request['workflow'].has_key?('name')
-            return [400, "No name specified for workflow"]
-          end
-          # Check if workflow_props exist and specfied as hash
-          if workflow_request['workflow'].has_key?('workflow_props')
-            if ! workflow_request['workflow']['workflow_props'].is_a? Hash
-              return [400, "workflow properties must be supplied as a hash"]
+        begin
+          if  workflow_request.has_key?('workflow')
+            #if no name attr specfied 
+            if ! workflow_request['workflow'].has_key?('name')
+              return [400, "No name specified for workflow"]
             end
-          end
-          #Check if job_ids specifeid and is a list
-          if workflow_request['workflow'].has_key?('job_ids')
-            if ! workflow_request['workflow']['job_ids'].is_a? Array
-              return [400, "job ids must be supplied as a list"]
+            # Check if workflow_props exist and specfied as hash
+            if workflow_request['workflow'].has_key?('workflow_props')
+              if ! workflow_request['workflow']['workflow_props'].is_a? Hash
+                return [400, "workflow properties must be supplied as a hash"]
+              end
+            end
+            #Check if job_ids specifeid and is a list
+            if workflow_request['workflow'].has_key?('job_ids')
+              if ! workflow_request['workflow']['job_ids'].is_a? Array
+                return [400, "job ids must be supplied as a list"]
+              end
+              
             end
             
+          else
+            #If no work flow key specified
+            return [400, 'No workflow key specified']
           end
-          
-        else
-          #If no work flow key specified
-          return [400, 'No workflow key specified']
+          return [202, workflow_request]
         end
-        return [202, workflow_request]
+      rescue
+        return [500, "An error has occurred"]
       end
+      
     
     end
   
